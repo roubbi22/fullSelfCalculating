f69ac04a638540c3e6eb8d93b0ebfecb539c1f58
roubbi22 <robinkr443@gmail.com>
2024-01-29 17:36:12 +0100

    feat: implement correct calculation for field winding area

--- functions/genrateLuaContent.js
+++ functions/genrateLuaContent.js
@@ -13,7 +13,22 @@ const genrateLuaContent = ({
   rotorCurrent,
   innerStatorRadius,
   airGap,
+  rotorWireDiameter,
+  rotorFillFactor,
+  FWyokeBaseFraction,
 }) => {
+  const calculatedFWHeight =
+    (Math.sqrt((innerStatorRadius - airGap) ** 2 - (yokeUpperWidth / 2) ** 2) -
+      youkeUpperOuterHeight -
+      Math.sqrt((outerDiameter / 2) ** 2 - (yokeBaseWidth / 2) ** 2) -
+      2 * padding) *
+    FWyokeBaseFraction;
+
+  const calculatedFWWidth =
+    (rotorTurns * (Math.PI * rotorWireDiameter ** 2)) /
+    4 /
+    (rotorFillFactor * calculatedFWHeight);
+
   return `innerDiameter = ${innerDiameter} --mm
 outerDiameter = ${outerDiameter} --mm
 yokeBaseWidth = ${yokeBaseWidth} --mm
@@ -21,8 +36,8 @@ yokeBaseHeight = ${yokeBaseHeight} --mm
 yokeUpperWidth = ${yokeUpperWidth} --mm
 youkeUpperOuterHeight = ${youkeUpperOuterHeight} --mm
 nYokes = ${nYokes}
-FWWidth = ${FWWidth} --mm
-FWHeight = ${FWHeight} --mm
+FWWidth = ${calculatedFWWidth} --mm
+FWHeight = ${calculatedFWHeight} --mm
 padding = ${padding} --mm
 rotorTurns = ${rotorTurns} --turns
 rotorCurrent = ${rotorCurrent} --A
